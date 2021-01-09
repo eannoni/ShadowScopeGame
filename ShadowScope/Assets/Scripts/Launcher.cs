8882d6a250df395766b7080ef05fe320688caa7d
Eli Annoni <annoni@chapman.edu>
2021-01-09 13:56:32 -0800

    Added visible players and start game functionality
    Also fixed some bugs and added performance boosts

--- ShadowScope/Assets/Scripts/Launcher.cs
+++ ShadowScope/Assets/Scripts/Launcher.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using Photon.Pun; //Photon Unity Networking
 using TMPro;
 using Photon.Realtime;
+using System.Linq;
 
 public class Launcher : MonoBehaviourPunCallbacks //gives access to callbacks for room creation, errors, joining lobbies, etc.
 {
@@ -12,8 +13,14 @@ public class Launcher : MonoBehaviourPunCallbacks //gives access to callbacks fo
     [SerializeField] TMP_InputField roomNameInputField;
     [SerializeField] TMP_Text errorText;
     [SerializeField] TMP_Text roomNameText;
+
     [SerializeField] Transform roomListContent;
     [SerializeField] GameObject roomListItemPrefab;
+
+    [SerializeField] Transform playerListContent;
+    [SerializeField] GameObject PlayerListItemPrefab;
+
+    [SerializeField] GameObject startGameButton;
 
     void Awake()
     {
@@ -33,12 +40,15 @@ public class Launcher : MonoBehaviourPunCallbacks //gives access to callbacks fo
         Debug.Log("Connected to Master");
         // lobbies are where you find/create rooms
         PhotonNetwork.JoinLobby();
+        // automatically loads scenes for all other clients when host changes scenes
+        PhotonNetwork.AutomaticallySyncScene = true;
     }
 
     public override void OnJoinedLobby()
     {
         Debug.Log("Joined Lobby");
         MenuManager.Instance.OpenMenu("title");
+        PhotonNetwork.NickName = "Player " + Random.Range(0, 1000).ToString("0000"); // temporary username system
     }
 
     public void CreateRoom()
@@ -57,6 +67,29 @@ public class Launcher : MonoBehaviourPunCallbacks //gives access to callbacks fo
     {
         MenuManager.Instance.OpenMenu("room");
         roomNameText.text = PhotonNetwork.CurrentRoom.Name;
+
+        Player[] players = PhotonNetwork.PlayerList;
+
+        // clears all players in playerListContent
+        foreach(Transform child in playerListContent)
+        {
+            Destroy(child.gameObject);
+        }
+
+        for (int i = 0; i < players.Count(); ++i)
+        {
+            Instantiate(PlayerListItemPrefab, playerListContent).GetComponent<PlayerListItem>().SetUp(players[i]);
+        }
+
+        // only lets the host be able to start the game
+        startGameButton.SetActive(PhotonNetwork.IsMasterClient);
+    }
+
+    // called if there is a host migration, which is if host leaves, another player becomes host
+    public override void OnMasterClientSwitched(Player newMasterClient)
+    {
+        // give new host access to start game button
+        startGameButton.SetActive(PhotonNetwork.IsMasterClient);
     }
 
     // failed to create room
@@ -64,6 +97,13 @@ public class Launcher : MonoBehaviourPunCallbacks //gives access to callbacks fo
     {
         errorText.text = "Room Creation Failed: " + message;
         MenuManager.Instance.OpenMenu("error");
+    }
+
+    public void StartGame()
+    {
+        // 1 is the scene index in the Build Settings (Game scene)
+        // this applies to all players in game, rather than unity loading the scene which would only trigger the host's scene
+        PhotonNetwork.LoadLevel(1);
     }
 
     public void LeaveRoom()
@@ -95,7 +135,16 @@ public class Launcher : MonoBehaviourPunCallbacks //gives access to callbacks fo
         // instantiate roomListItemPrefabs inside RoomListItem container
         for (int i = 0; i < roomList.Count; ++i)
         {
+            // if roomListItem has been removed, don't instantiate
+            if(roomList[i].RemovedFromList)
+                continue;
+
             Instantiate(roomListItemPrefab, roomListContent).GetComponent<RoomListItem>().SetUp(roomList[i]);
         }
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Instantiate(PlayerListItemPrefab, playerListContent).GetComponent<PlayerListItem>().SetUp(newPlayer);
     }
 }
