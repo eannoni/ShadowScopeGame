c928a881bc26d83ce23283e1b925ce68631e12eb
Keoni Lanoza <lanoza@chapman.edu>
2021-01-12 18:01:23 -0800

    Added shooting - direction needs fixing
    Gun object attached to mainCamera might not be needed since I moved the script into PlayerController.cs
    
    Gun script attached to PlayerController might also not be necessary.
    
    Both of these are disabled atm.

---
+++ ShadowScope/Assets/Scripts/Gun.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+    public float damage = 10f;
+    public float range = 10000f;
+
+    public Camera fpsCam;
+    Vector2 mousePos;
+    float rotationSpeed = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) // Left click fires
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit)) // position, direction, output to variable hit, and range
+        {
+            // Section does not work as far as I've tested.
+            Debug.Log(hit.transform.name); // Testing if a raycast has hit another object with a collider.
+        }
+
+        Debug.Log(fpsCam.transform.forward); // Prints direction fired
+
+        Debug.Log("Fired a shot"); // This works if I left-click
+
+        // Copied below from PlayerController for rotation
+
+        Vector2 lookDirection = mousePos;
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        //transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);  
+        Debug.Log(angle); // To see if the shooting is changing direction
+        //Debug.Log(transform.rotation);
+        //Debug.Log(rotation);
+    }
+}
--- ShadowScope/Assets/Scripts/PlayerController.cs
+++ ShadowScope/Assets/Scripts/PlayerController.cs
@@ -47,6 +47,11 @@ public class PlayerController : MonoBehaviour
         sprinting = Input.GetKey(KeyCode.LeftShift);
 
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetButtonDown("Fire1")) // Left click fires
+        {
+            Shoot();
+        }
     }
 
     void FixedUpdate()
@@ -80,5 +85,25 @@ public class PlayerController : MonoBehaviour
             body.velocity = new Vector2(horizontal * sprintSpeed, vertical * sprintSpeed);
         else
             body.velocity = new Vector2(horizontal * walkSpeed, vertical * walkSpeed);
+    }
+
+    void Shoot()
+    {
+        Vector2 lookDirection = mousePos;
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);  
+        RaycastHit hit;
+
+        if (Physics.Raycast(this.transform.position, transform.rotation.ToEulerAngles(), out hit)) // position, direction, output to variable hit, and range
+        {
+            // Section does not work as far as I've tested.
+            Debug.Log("Hit: " + hit.transform.name); // Prints the name of the object it hits if successful.
+            Debug.Log("Hit something"); // Tests if a collision was detected even if the object has no name.
+        }
+
+        Debug.Log("Origin of raycast:" + this.transform.position); // Prints raycast origin into console and confirms the raycast was sucessful
+        Debug.Log("Direction:" + transform.rotation.ToEulerAngles()); // Prints the direction of the raycast.
+
     }
 }
