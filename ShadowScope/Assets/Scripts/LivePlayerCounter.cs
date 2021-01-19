961098a053433d9a5f7b85c893a78fd176d03bbd
Ian Hakeman <69366771+ihakeman@users.noreply.github.com>
2021-01-18 22:55:57 -0800

    Added back button to create room page. Moved start game button. Added live player counter.

---
+++ ShadowScope/Assets/Scripts/LivePlayerCounter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LivePlayerCounter : MonoBehaviour
+{
+    public GameObject[] activePlayers;
+    public TextMeshProUGUI numActivePlayers;
+    void Update()
+    {
+        activePlayers = GameObject.FindGameObjectsWithTag("Player");
+        numActivePlayers.text = "Live Players: " + activePlayers.Length;
+    }
+}
