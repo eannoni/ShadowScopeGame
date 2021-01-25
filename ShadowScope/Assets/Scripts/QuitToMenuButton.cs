d59bd9936a431de39c4d6764217b93363688a6dd
Eli Annoni <annoni@chapman.edu>
2021-01-25 11:02:33 -0800

    Updated quit menu button

--- ShadowScope/Assets/Scripts/QuitToMenuButton.cs
+++ ShadowScope/Assets/Scripts/QuitToMenuButton.cs
@@ -8,7 +8,14 @@ public class QuitToMenuButton : MonoBehaviour
 {
     public void OnQuitClick()
     {
-        PhotonNetwork.Disconnect();
+        StartCoroutine(DisconnectAndLoad());
+    }
+
+    IEnumerator DisconnectAndLoad()
+    {
+        PhotonNetwork.LeaveRoom();
+        while (PhotonNetwork.InRoom)
+            yield return null;
         SceneManager.LoadScene(0);
     }
 }
